97864343894bc0a24d98da6fb9bb1b722fcb352f
Tahabaig044 <135549410+Tahabaig044@users.noreply.github.com>
2025-06-13 00:36:12 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -53,17 +53,17 @@ const Navbar = () => {
 
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex gap-2 justify-between items-center bg-white max-sm:px-2">
-      <Link href="/">
-        <div className="rounded-xl">
-        <Image
-          src="/IMG-20250611-WA0026.jpg"
-          alt="Website logo"
-          width={100}
-          height={70}
-          className="lg:w-[50%]"
-        />
-          </div>
-      </Link>
+      <Link href="/" className="flex items-center">
+  <div className="relative w-[120px] h-[60px] sm:w-[140px] sm:h-[70px]">
+    <Image
+      src="/IMG-20250611-WA0026.jpg"
+      alt="Website logo"
+      fill
+      className="object-contain"
+    />
+  </div>
+</Link>
+
 
       <div className="flex gap-4 text-base-bold max-lg:hidden w-[30%]">
         <Link
